0dbd3a6c2b899b8b21df109e1da37bbaa04cdb38
Andrei <Andy@mail.com>
2021-09-11 16:35:54 +0300

    start mdb

---
+++ script.js
@@ -0,0 +1,20 @@
+const numberOfFilms = prompt('Сколько фильмов посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Последний просмотренный фильм?', ''),
+    b = prompt('Его оценка?', ''),
+    c = prompt('Последний просмотренный фильм?', ''), 
+    d = prompt('Его оценка?', '');
+
+personalMovieDB.movies[a] = [b];
+personalMovieDB.movies[c] = [d];
+
+console.log(personalMovieDB);
+    
