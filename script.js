0f5a3845a75f34e25b4c905845756e95ac299974
Andrei <Andy@mail.com>
2021-09-22 17:50:51 +0300

    mdb 2

--- script.js
+++ script.js
@@ -8,13 +8,28 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Последний просмотренный фильм?', ''),
-    b = prompt('Его оценка?', ''),
-    c = prompt('Последний просмотренный фильм?', ''), 
-    d = prompt('Его оценка?', '');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Последний просмотренный фильм?', ''),
+          b = prompt('Его оценка?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('da');
+    } else {
+        i--;
+        console.log('net');
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Ошибка');
+}
 
 console.log(personalMovieDB);
     
